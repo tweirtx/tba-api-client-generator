2b2b8b413f1fb157f9af7b0da2c356da5fe8a47a
Travis Weir <tweirtx@gmail.com>
2020-06-06 14:17:45 -0500

    Update swiftenv.py

--- swiftenv.py
+++ swiftenv.py
@@ -1,8 +1,6 @@
 import os
 if os.getenv('OUTLANG') == 'swift':
     genlang = 'swift4'
-elif os.getenv('OUTLANG') == 'dart':
-    genlang = 'dart-jaguar'
 else:
     genlang = os.getenv('OUTLANG')
 
